75fccc5a66f7beea54f4461b6e628c5657132cbe
Joey <joeychung960@gmail.com>
2017-11-27 09:23:38 -0800

    Leo please cut my hair

--- Day02/ex01/ft_print_reverse_alphabet.c
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -10,16 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int			ft_putchar(char c);
-
 void		ft_print_reverse_alphabet(void)
 {
-	char c;
-
-	c = 'z';
-	while (c >= 'a')
-	{
-		ft_putchar(c);
-		c--;
-	}
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
 }
